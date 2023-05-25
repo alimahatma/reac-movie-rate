42267026a7e85af1b035cb28e78d5509f4e0e888
ali <laluallistilo@gmail.com>
2023-05-25 11:00:58 +0800

    / code for list and Query RestAPI from Server   // in this is case i'm using Djago framewor become RestAPi

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
 
-  const [movies, setMovie]= useState(['Movie 1','Movie 2']);
+  const [movies, setMovie]= useState([]);
+
+
+  // code for list and Query RestAPI from Server 
+  // in this is case i'm using Djago framewor become RestAPi
+
+  useEffect(()=>{
+    fetch("http://127.0.0.1:8000/api/movies/", {
+		method: 'GET',
+		header: {
+			'Content-Type':'application/jason',
+			'Authorization':'Token 7a09e31c3b97cddec2a066564fbb82a6fbe4f4a9'
+		}
+	})
+  .then( resp => resp.json())
+	.then(resp => setMovie(resp))
+	.catch(error => console.log(error))
+  }, [])
 
   return (
     <div className="App">
